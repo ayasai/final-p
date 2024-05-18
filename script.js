76777efb40c81e81749cea3e6645f4de3265b51e
Aya <ayal1ghtt@mail.ru>
2024-05-18 11:32:10 +0500

    условие, которое проверяет длину массива results, ели массив пустой, то результатов не найдено, иначе resultsDiv.innerHTML = '<p>No results found</p>: Если массив results пустой, это означает, что не было найдено результатов. В этом случае код устанавливает содержимое элемента resultsDiv в строку '<p>No results found</p>', чтобы отобразить сообщение о том, что результаты не найдены. метод forEach() перебирает каждый элемент массива results. resultItem.classList.add('result-item');: Добавляется класс result-item к созданному элементу.

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ document.getElementById('search-button').addEventListener('click', function() {
 function displayResults(results) {
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
+
+    
+  if (results.length === 0) {
+    resultsDiv.innerHTML = '<p>No results found</p>';
+} else {
+    results.forEach(result => {
+        const resultItem = document.createElement('div');
+        resultItem.classList.add('result-item');
+        resultItem.innerHTML = `
+            <h3>${result.name}</h3>
+            <p>ID: ${result.id}</p>
+            <p>Height: ${result.height}</p>
+            <p>Weight: ${result.weight}</p>
+            <img src="${result.sprites.front_default}" alt="${result.name}">
+        `;
+        resultsDiv.appendChild(resultItem);
+    });
+}
+}
