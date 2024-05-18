9635c0e0efafaa6d9d44ac9c7b65b5f915af9e08
Aya <ayal1ghtt@mail.ru>
2024-05-18 11:25:56 +0500

    метод trim() удаляет пробелы с начала и конца строки. Ключевое слово return завершает выполнение функции и возвращает управление вызывающей стороне. Если условие истинно, выполнение функции прекращается, и дальнейший код не выполняется. Таким образом, если введённое значение пустое или содержит только пробелы, выводится предупреждение, и выполнение функции прекращается.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.getElementById('search-button').addEventListener('click', function() {
     const query = document.getElementById('search-input').value.toLowerCase();
-    
+  
+    if (query.trim() === '') {
+        alert('Please enter a Pokemon name.');
+        return;
+    }
+  
+  if (query.trim() === '') {
+      alert('Please enter a Pokemon name.');
+      return;
+  }
+  
